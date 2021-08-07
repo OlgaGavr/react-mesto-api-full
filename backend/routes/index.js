08b3c81b53ac1a134c4d76b38cd21d4b241d0067
Olga Gavrilova <olgavrilova@gmail.com>
2021-08-07 19:05:57 +0200

    исправить эндпоинты3

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,8 +4,8 @@ const NotFoundError = require('../errors/not-found-err');
 const userRouters = require('./users');
 const cardRouters = require('./cards');
 
-router.use('/users', userRouters);
-router.use('/cards', cardRouters);
+router.use('/api/users', userRouters);
+router.use('/api/cards', cardRouters);
 
 router.use((req, res, next) => {
   const err = new NotFoundError('Запрашиваемый ресурс не найден');
